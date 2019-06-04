b4be78a57293524a4014130355e089174c7e096b
Flatout73 <fantom2259@gmail.com>
2019-06-04 13:46:04 +0300

    Zone fixes

--- src/main/java/ru/hse/BikeSharing/controller/ZoneController.java
+++ src/main/java/ru/hse/BikeSharing/controller/ZoneController.java
@@ -1,16 +1,18 @@
 package ru.hse.BikeSharing.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.hse.BikeSharing.domain.Alert;
 import ru.hse.BikeSharing.domain.RestrictedZone;
+import ru.hse.BikeSharing.errors.NotFoundException;
 import ru.hse.BikeSharing.repo.AlertRepo;
 import ru.hse.BikeSharing.repo.RestrictedZoneRepo;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/zone")
+@RequestMapping("zone")
 public class ZoneController {
 
     @Autowired
@@ -30,5 +32,14 @@ public class ZoneController {
     @GetMapping("alerts")
     public List<Alert> getAlerts() {
         return alertRepo.findAll();
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<String> deleteZone(@RequestParam Long zoneID) {
+        RestrictedZone zone = restrictedZoneRepo.findById(zoneID).orElseThrow(()->new NotFoundException("Zone not found"));
+
+        restrictedZoneRepo.delete(zone);
+
+        return ResponseEntity.ok("Success");
     }
 }
